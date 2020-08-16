b1f78dce6b278e199912d63fa222470c736a1643
Mikel Maron <mikel_maron@yahoo.com>
2020-08-16 15:05:54 -0400

    decimal encode

--- mailbox/list.py
+++ mailbox/list.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from mailbox import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
@@ -14,7 +15,7 @@ def list(event, context):
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['Items'])
+        "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
     }
 
     return response
